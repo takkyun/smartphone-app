7cc4d481d8aed7318ae7c573b4a25e2b6b75f852
Takuya Otani / SimpleBoxes <takuya.otani@serenebach.net>
2015-10-15 22:44:26 +1300

    :lipstick: cosmetic changes #132
    Remove redundant comments.

--- MT_iOS/MT_iOS/Classes/ViewController/EditorModeTableViewController.swift
+++ MT_iOS/MT_iOS/Classes/ViewController/EditorModeTableViewController.swift
@@ -20,12 +20,6 @@ class EditorModeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
         self.title = NSLocalizedString("EditorMode", comment: "EditorMode")
         
         selected = oldSelected
@@ -69,57 +63,14 @@ class EditorModeTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         selected = Entry.EditMode(rawValue: indexPath.row)!
         
         self.tableView.reloadData()
     }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
+
+    // MARK: - actions
+
     @IBAction func saveButtonPushed(sender: UIBarButtonItem) {
         delegate?.editorModeDone(self, selected: selected)
         self.navigationController?.popViewControllerAnimated(true)
